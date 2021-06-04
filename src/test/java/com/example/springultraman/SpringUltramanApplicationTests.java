e77b4048ad9efcd7d32e55520c44a8ddd367681b
jiangyusong <jiangyusong@kayak.com.cn>
2021-06-04 16:44:20 +0800

    spring-ultraman first

---
+++ src/test/java/com/example/springultraman/SpringUltramanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springultraman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUltramanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
